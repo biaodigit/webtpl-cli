5c346eb5516eae842a9206b8ff259d57dba40d5e
luowendi <luowendi1996@gmail.com>
2020-11-10 12:28:13 +0800

    feat:react-template jest configuration

--- src/config/getPrompts.js
+++ src/config/getPrompts.js
@@ -1,70 +1,77 @@
-const basePrompt = (name) => ([
-    {
-        name: 'name',
-        message: '项目的名称',
-        default: name
-    }, {
-        name: 'version',
-        message: '项目的版本号',
-        default: '1.0.0'
-    }, {
-        name: 'description',
-        message: '项目的简介',
-        default: `${name} project`
-    }
-])
+const basePrompt = (name) => [
+  {
+    name: 'name',
+    message: '项目的名称',
+    default: name
+  },
+  {
+    name: 'version',
+    message: '项目的版本号',
+    default: '1.0.0'
+  },
+  {
+    name: 'description',
+    message: '项目的简介',
+    default: `${name} project`
+  }
+]
 
 const reactPrompt = [
-    {
-        type: "confirm",
-        message: "使用typescript?",
-        name: "typescript"
-    },
-    {
-        type: "confirm",
-        message: "使用redux?",
-        name: "redux"
-    },
-    {
-        type: "confirm",
-        message: "使用react-router?",
-        name: "react_router"
-    },
-    {
-        type: "confirm",
-        message: "使用预处理器?",
-        name: "use_csspre",
-    },
-    {
-        type: "list",
-        message: "请选择 CSS 预处理器(Sass/Less/Stylus)",
-        name: "css_pre",
-        choices: [
-            { name: "Sass", value: "sass" },
-            { name: "Less", value: "less" },
-            { name: "Stylus", value: "stylus" }
-        ],
-        when: (res) => Boolean(res.use_csspre)
-    }
+  {
+    type: 'confirm',
+    message: '使用typescript?',
+    name: 'typescript'
+  },
+  {
+    type: 'confirm',
+    message: '使用redux?',
+    name: 'redux'
+  },
+  {
+    type: 'confirm',
+    message: '使用react-router?',
+    name: 'react_router'
+  },
+  {
+    type: 'confirm',
+    message: '使用预处理器?',
+    name: 'use_csspre'
+  },
+  {
+    type: 'list',
+    message: '请选择 CSS 预处理器(Sass/Less/Stylus)',
+    name: 'css_pre',
+    choices: [
+      { name: 'Sass', value: 'sass' },
+      { name: 'Less', value: 'less' },
+      { name: 'Stylus', value: 'stylus' }
+    ],
+    when: (res) => Boolean(res.use_csspre)
+  },
+  {
+    type: 'confirm',
+    message: '使用自动化测试(jest)?',
+    name: 'jest'
+  }
 ]
 
 const vuePrompt = []
 
 const rollupPrompt = [
-    {
-        type: "confirm",
-        message: "使用typescript?",
-        name: "typescript"
-    }
+  {
+    type: 'confirm',
+    message: '使用typescript?',
+    name: 'typescript'
+  }
 ]
 
 const promptMap = new Map([
-    ['react', reactPrompt],
-    ['vue', vuePrompt],
-    ['rollup', rollupPrompt]
+  ['react', reactPrompt],
+  ['vue', vuePrompt],
+  ['rollup', rollupPrompt]
 ])
 
 module.exports = (name, type) => {
-    const customizePrompt = promptMap.get(type.replace(/template-/, ''))
-    return [...basePrompt(name), ...customizePrompt]
+  const customizePrompt = promptMap.get(type.replace(/template-/, ''))
+  return [...basePrompt(name), ...customizePrompt]
 }
